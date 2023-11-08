5a388c162418166afa1f84c15d7c0c8088cd057a
masha <m@matyuha.ru>
2023-11-08 23:13:31 +0300

    added NegativeTest and minimalFillingForm

--- src/test/java/tests/RegistrationFormTest.java
+++ src/test/java/tests/RegistrationFormTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
+import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverConditions.url;
 
 public class RegistrationFormTest {
     @BeforeAll
@@ -53,5 +55,33 @@ public class RegistrationFormTest {
                 text("pic.jpg"),
                 text("New Delhi"),
                 text("NCR Delhi"));
+    }
+
+    @Test
+    void minimalFillingForm() {
+        open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+        $("#firstName").setValue("Alex");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Alexivanov@gmail.ru");
+        $("#gender-radio-1").doubleClick();
+        $("#userNumber").setValue("9998887766");
+        $("#submit").pressEnter();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Alex Ivanov"),
+                text("Alexivanov@gmail.ru"),
+                text("Male"),
+                text("9998887766"));
+    }
+
+    @Test
+    void formNegativeTest() {
+        open("/automation-practice-form");
+        $("#submit").pressEnter();
+        webdriver().shouldHave(url("https://demoqa.com/automation-practice-form"));
+        $("#firstName").shouldHave(cssValue("border-color","rgb(220, 53, 69)"));
+        $("#lastName").shouldHave(cssValue("border-color","rgb(220, 53, 69)"));
     }
 }
