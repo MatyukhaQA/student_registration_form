b66bbcee1cf6efb2fedb203c4e71383a323bb54c
masha <m@matyuha.ru>
2023-12-03 12:27:46 +0300

    added closeWebDrive

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -29,10 +30,11 @@ public class TestBase {
     }
 
     @AfterEach
-    void addAttachments() {
+    void afterEach() {
         Attach.screenshotAs("Last screenshot");
         Attach.pageSource();
         Attach.browserConsoleLogs();
         Attach.addVideo();
+        Selenide.closeWebDriver();
     }
 }
